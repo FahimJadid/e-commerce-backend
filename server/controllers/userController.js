7b7155d97194f6372fd15e7aa6585f6167ce8ac6
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-02 05:57:13 +0600

    implemented login user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -13,4 +13,25 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // find the user by email:
+  const findUser = await User.findOne({ email });
+
+  // Check if user exists:
+  if (!findUser) {
+    throw new Error("User does not exist");
+  }
+
+  // Check if password matches:
+  const isPasswordMatch = await findUser.comparePassword(password);
+
+  if (isPasswordMatch) {
+    res.json({ message: "Login successful", user: findUser });
+  } else {
+    throw new Error("Invalid Credentials");
+  }
+});
+
+module.exports = { createUser, loginUser };
