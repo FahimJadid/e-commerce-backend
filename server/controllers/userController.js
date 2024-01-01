b2c0ba7874a22570f06b498f54dc08b88318d35e
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-02 02:05:59 +0600

    created create user controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,26 @@
+const User = require("../models/UserModel");
+
+const createUser = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const findUser = await User.findOne({ email });
+
+    if (!findUser) {
+      const newUser = User.create(req.body);
+      res.json(newUser);
+    } else {
+      res.json({
+        message: "User already exists",
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.error("Error creating user: ", error.message);
+    res.json({
+      message: "Error creating user",
+      success: false,
+    });
+  }
+};
+
+module.exports = { createUser };
