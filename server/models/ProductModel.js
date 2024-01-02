dd230eda2259de51ad6189b3b6a25d294e131a07
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-03 05:08:14 +0600

    modified product model

--- server/models/ProductModel.js
+++ server/models/ProductModel.js
@@ -29,34 +29,21 @@ const productSchema = new Schema(
       required: true,
     },
     category: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
+      //   type: mongoose.Schema.Types.ObjectId,
+      //   ref: "Category",
+      type: String,
+      required: true,
     },
     brand: {
       type: String,
-      enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS", "Other"],
+      required: true,
     },
     images: {
       type: Array,
     },
     color: {
       type: String,
-      enum: [
-        "white",
-        "black",
-        "blue",
-        "red",
-        "green",
-        "yellow",
-        "orange",
-        "pink",
-        "brown",
-        "purple",
-        "gray",
-        "silver",
-        "gold",
-        "other",
-      ],
+      required: true,
     },
     ratings: [
       {
