5de8d5a04b9a3736aa5314ff2ab2dece79543c8e
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-04 05:44:03 +0600

    blog model

---
+++ server/models/BlogModel.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    numViews: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    dislikes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    Image: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1623031345438-387dd3c45e9f?q=80&w=1633&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    author: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
