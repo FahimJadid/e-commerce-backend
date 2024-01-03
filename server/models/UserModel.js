94e24472c636e274d9e2325c33d3564fd0f27606
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-03 23:10:25 +0600

    reset password token

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,74 +1,77 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 const { Schema } = mongoose;
 
 // User Model
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, "First Name required"],
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    required: [true, "First Name required"],
-    trim: true,
-  },
-  mobile: {
-    type: String,
-    required: [true, "Mobile number required"],
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Email required"],
-    trim: true,
-    lowercase: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password required"],
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  cart: {
-    type: Array,
-    default: [],
-  },
-  isBlocked: {
-    type: Boolean,
-    default: false,
-  },
-  address: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Address",
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First Name required"],
+      trim: true,
     },
-  ],
-  wishlist: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Product",
+    lastName: {
+      type: String,
+      required: [true, "First Name required"],
+      trim: true,
     },
-  ],
-  refreshToken: {
-    type: String,
+    mobile: {
+      type: String,
+      required: [true, "Mobile number required"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email required"],
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password required"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    address: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Address",
+      },
+    ],
+    wishlist: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    refreshToken: {
+      type: String,
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
-  timestamp: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  { timestamps: true }
+);
 
 // Middleware to hash the password before saving
 userSchema.pre("save", async function (next) {
   try {
-    if (!this.isModified("password")) return next();
+    if (!this.isModified("password")) return next(); // If password is not modified, continue
 
     const hashedPassword = await bcrypt.hash(this.password, 10);
     this.password = hashedPassword;
@@ -85,6 +88,18 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
   } catch (error) {
     throw error;
   }
+};
+
+// Reset Password Token
+userSchema.methods.createPasswordResetToken = async function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
+  return resetToken;
 };
 
 module.exports = mongoose.model("User", userSchema);
